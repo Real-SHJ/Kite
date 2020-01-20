6451483d39c3fde39aa1b111f7bd928531ba44fa
AlBan <albaneo0724@gmail.com>
2020-01-20 09:46:21 +0900

    configuration update

--- FrontEnd/kite_front/.eslintrc.js
+++ FrontEnd/kite_front/.eslintrc.js
@@ -1,39 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+    'no-console':'warn',
+    'linebreak-style':['error','windows'],
+  },
 };
