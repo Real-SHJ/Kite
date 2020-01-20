f9c97202006328805c2a8be9bc2e1ef09ca7dc13
AlBan <albaneo0724@gmail.com>
2020-01-20 09:03:08 +0900

    alsdf

--- Backend/kite/src/main/java/com/wildbody/kite/Repository/MemberRepoImpl.java
+++ Backend/kite/src/main/java/com/wildbody/kite/Repository/MemberRepoImpl.java
@@ -1,3 +1,6 @@
 package com.wildbody.kite.Repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberRepoImpl implements IMemberRepo {}
--- Backend/kite/src/main/java/com/wildbody/kite/Service/IMemberService.java
+++ Backend/kite/src/main/java/com/wildbody/kite/Service/IMemberService.java
@@ -1,3 +1,5 @@
 package com.wildbody.kite.Service;
 
+import org.springframework.stereotype.Service;
+
 public interface IMemberService {}
