a35bba5f7c872f7dcf43181ae26a1439d302fa1d
AlBan <albaneo0724@gmail.com>
2020-01-22 23:17:17 +0900

    merge process

---
+++ FrontEnd/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-content>
+    <v-toolbar color="#FBFAF5" class="d-flex flex-row-reverse">
+      <router-link class="black--text" to="/">Home</router-link> |
+      <router-link class="black--text" to="/loginPage">Login</router-link>|
+      <router-link class="black--text" to="/signUp">SignUp</router-link>
+    </v-toolbar>
+  </v-content>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
