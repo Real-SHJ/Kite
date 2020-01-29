99d1198a4ac2e4c44b35e3ca12ba44077624d950
AlBan <albaneo0724@gmail.com>
2020-01-29 15:35:39 +0900

    image server upload

---
+++ Backend/imgserver/src/main/java/edu/wildbody/imgserver/Config.java
@@ -0,0 +1,34 @@
+package edu.wildbody.imgserver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    private final String uploadImagePath;
+
+    public Config(@Value("${custom.path.upload-images}")String uploadImagePath){
+        this.uploadImagePath = uploadImagePath;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        List<String> imagesFolders = Arrays.asList("");
+
+        for(String imageFolder : imagesFolders){
+        registry.addResourceHandler("/img/" + imageFolder +"/**")
+            .addResourceLocations("file:///"+uploadImagePath+imageFolder+"/")
+            .setCachePeriod(3600)
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver());
+
+        }
+    }
+}
