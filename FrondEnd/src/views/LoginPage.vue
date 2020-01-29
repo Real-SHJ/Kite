270b884f20bedf77cf51f8ef4214690aa42aa9a8
bumbum9944 <tong940526@gmail.com>
2020-01-29 11:34:07 +0900

    loginpage add

---
+++ FrondEnd/src/views/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-content>
+    <h1>여기는 로그인 페이지입니다.</h1>
+    <LoginForm/>
+  </v-content>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  name: 'login',
+
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+
+</style>
