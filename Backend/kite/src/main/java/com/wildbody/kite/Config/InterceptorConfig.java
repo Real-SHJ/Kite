ff688e43dcb24d0f427d066ad34c191b48b0303a
AlBan <albaneo0724@gmail.com>
2020-01-29 17:18:08 +0900

    backend update

--- Backend/kite/src/main/java/com/wildbody/kite/Config/InterceptorConfig.java
+++ Backend/kite/src/main/java/com/wildbody/kite/Config/InterceptorConfig.java
@@ -1,6 +1,8 @@
 package com.wildbody.kite.Config;
 
 import com.wildbody.kite.Interceptor.AuthInterceptor;
+import com.wildbody.kite.Interceptor.KakaoInterceptor;
+import com.wildbody.kite.Interceptor.NaverInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,15 +13,29 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     private static final String[] EXCLUDE_PATH = {"/api/article/**"};
     // 우선은 멤버관련 경로로 접근하는 경우에만 체크를 해보자
-    private static final String[] INCLUDE_PATH={"/api/member/**"};
+    private static final String[] INCLUDE_PATH = {"/api/member/**"};
 
     @Autowired
     private AuthInterceptor interceptor;
+
+    @Autowired
+    private NaverInterceptor naverInterceptor;
+
+    @Autowired
+    private KakaoInterceptor kakaoInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor)
             .addPathPatterns(INCLUDE_PATH)    /* 유저 인증이 필요한 경로 */
             .excludePathPatterns(EXCLUDE_PATH);     /* 유저 인증이 필요하지 않은 경로 */
+
+        registry.addInterceptor(naverInterceptor)
+            .addPathPatterns("/api/member/naverlogin")
+            .excludePathPatterns(EXCLUDE_PATH);
+
+        registry.addInterceptor(kakaoInterceptor)
+            .addPathPatterns("/api/member/kakaologin")
+            .excludePathPatterns(EXCLUDE_PATH);
     }
 }
--- Backend/kite/src/main/java/com/wildbody/kite/Dto/KakaoMember.java
+++ Backend/kite/src/main/java/com/wildbody/kite/Dto/KakaoMember.java
@@ -1,5 +1,9 @@
 package com.wildbody.kite.Dto;
 
+import lombok.Data;
+
+@Data
 public class KakaoMember {
     private String accesstoken;
+    private String refreshtoken;
 }
