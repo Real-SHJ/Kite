af4c581ef216030e0585486598ec4b96d8576b0a
AlBan <albaneo0724@gmail.com>
2020-01-30 18:03:36 +0900

    Dev (#30)
    * ImageServer modified (#21)
    
    * delete unused source
    
    * sever update
    
    * sql query update
    
    * backend source code update for aws
    
    * modify configure file
    
    * min [+] edit PersonalDataModify.vue / [+] edit SignUp.vue  / [+] edit  http-common.js (#22)
    
    * 기사 카트 구성 수정 app.vue 스타일 수정 (#23)
    
    * search bar add (#29)
    
    * asdkjasd
    
    Co-authored-by: Minnaldo <minnaldo6602@gmail.com>
    Co-authored-by: bumbum9944 <52690508+bumbum9944@users.noreply.github.com>
    Co-authored-by: wontaehee <fevernovo@hanmail.net>

--- FrontEnd/src/components/Menu2.vue
+++ FrontEnd/src/components/Menu2.vue
@@ -1,36 +1,42 @@
 <template>
   <v-layout class="layout">
 
-      <v-btn class="mx-2" fab dark color="teal" @mouseover="drawer = true">
+      <v-btn class="mx-2" fixed fab dark color="teal" @mouseover="drawer = true">
       <v-icon dark>mdi-format-list-bulleted-square</v-icon>
     </v-btn>
 
     <v-navigation-drawer
+    class="nav-bar"
       v-model="drawer"
-      absolute
+      fixed
       temporary
+      width="350px"
       @mouseleave.native="drawer = false"
     >
       <v-list-item>
         <v-list-item-avatar>
+          <!-- info.img를 추가 v-img에다가 "https:// /api/list-->
           <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
         </v-list-item-avatar>
 
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
+        <v-list-item-content >
+          <v-list-item-title >John Leider</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-
       <v-divider></v-divider>
-
-      <v-list dense>
+       <v-input @keyup="search" v-model="searchText" name="searchText">🔎
+         <v-text-field placeholder="검색어를 입력해주세요"></v-text-field>
+       </v-input>
+      <v-divider></v-divider>
+      <!-- 밑에 백엔드 연결할 것... -->
+      <v-list dense >
         <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-avatar>
-            <v-img src="../assets/logo.png"></v-img>
+          <v-list-item-avatar >
+            <v-img src="http://13.125.153.118:8999/img/logo/samsung.png" ></v-img>
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title >{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -39,15 +45,56 @@
 </template>
 
 <script>
+import http from '../http-common'
+// import images from '../http-images'
 export default {
   data () {
     return {
       drawer: null,
+      picture: [],
+      info: [],
+      info2: [],
       items: [
-        { title: '삼성전자', icon: '../assets/samsung.png' },
-        { title: 'LG전자', icon: 'question_answer' }
+        { title: '삼성전자', url: 'http://13.125.153.118:8999/img/logo/samsung.png' },
+        { title: 'LG전자', url: 'http://13.125.153.118:8999/img/logo/LGElec.png' },
+        { title: 'SK텔레콤', url: 'http://13.125.153.118:8999/img/logo/SKtelecom.png' },
+        { title: 'GS칼텍스', url: 'http://13.125.153.118:8999/img/logo/GScaltex.png' },
+        { title: 'KT', url: 'http://13.125.153.118:8999/img/logo/KT.png' },
+        { title: '네이버', url: 'http://13.125.153.118:8999/img/logo/NAVER.png' },
+        { title: 'S-OIL', url: 'http://13.125.153.118:8999/img/logo/S-OIL.png' },
+        { title: 'SK하이닉스', url: 'http://13.125.153.118:8999/img/logo/SKhynix.png' },
+        { title: '현대자동차', url: 'http://13.125.153.118:8999/img/logo/HyundaiCar.png' },
+        { title: 'CJ제일제당', url: 'http://13.125.153.118:8999/img/logo/CJJJ.png' }
       ]
     }
+  },
+  mounted: {
+  },
+  methods: {
+    init () {
+      http
+        .get('http:')
+        .then(response => (this.info = response.data.resvalue))
+        .catch(() => {
+          this.errored = true
+        })
+        .finally(() => (this.loading = false))
+    }
+  },
+  search () {
+    http
+      .get('/productinfo' + '/' + this.searchText) // rest에서 가져올 절대 주소 적기
+      .then(response => {
+        this.info = response.data.resvalue
+        this.info2 = response.data.resvalue2
+        for (let index = 0; index < this.info.length; index++) {
+          this.info[index].img = '/' + this.info[index].img
+        }
+      })
+      .catch(() => {
+        this.errored = true
+      })
+      .finally(() => (this.loading = false))
   }
 }
 </script>
@@ -57,10 +104,10 @@ export default {
   width: 64px;
 }
 .btn_abc {
-  margin-top: 200px;
+  top: 200px;
   width: 50px;
 }
 .v-overlay {
-  opacity: 0;
+  opacity: 0.1;
 }
 </style>
---
+++ FrontEnd/src/views/SelectCompany.vue
@@ -0,0 +1,138 @@
+<template>
+  <v-content>
+    <v-card class="mx-auto">
+        <v-container class="pa-1">
+            <v-row class="fill-height" align="center" justify="center">
+              <template v-for="(item, i) in items">
+                <v-col :key="i" md="4">
+                  <v-hover v-slot:default="{ hover }">
+                    <v-card :elevation="hover ? 12 : 2" :class="{ 'on-hover': hover }" @click="handleClick(i)">
+                      <v-img :src="item.src" height="100px" width="287px">
+                        <v-card-title class="title white--text">
+                          <v-row class="fill-height flex-column" justify="space-between">
+                            <div class="text-right float-left">
+                              <v-btn :class="{ 'show-btns': hover }" class="mx-2 clickbtn" fab dark small color="red" icon @click="handleClick(i)">
+                                <v-icon v-if="!item.clicked" color="balck">{{ 'mdi-heart-outline' }}</v-icon>
+                                <v-icon v-else color="red">{{ 'mdi-heart' }}</v-icon>
+                              </v-btn>
+                            </div>
+                          </v-row>
+                        </v-card-title>
+                      </v-img>
+                    </v-card>
+                  </v-hover>
+                </v-col>
+              </template>
+            </v-row>
+        </v-container>
+    </v-card>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: 'selectcompany',
+  data: () => ({
+    items: [
+      {
+        src: require('../assets/company_logo/국민은행.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/기업은행.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/삼성전자.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/삼성SDS.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/신한은행.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/우리은행.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/쿠팡.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/포스코.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/한국전력공사.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/현대자동차.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/CJ제일제당.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/GS칼텍스.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/KT.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/LG전자.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/NAVER.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/S-Oil.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/SK텔레콤.png'),
+        clicked: false
+      },
+      {
+        src: require('../assets/company_logo/SK하이닉스.png'),
+        clicked: false
+      }
+    ]
+  }),
+  methods: {
+    handleClick: function (index) {
+      var items = this.items
+      for (let i = 0; i < items.length; i++) {
+        if (i === index) {
+          if (items[i].clicked === false) { items[i].clicked = true } else items[i].clicked = false
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 1;
+  position: absolute;
+}
+.v-card {
+transition: opacity .2s ease-in-out;
+}
+.v-card:not(.on-hover) {
+opacity: 0.75;
+ }
+</style>
