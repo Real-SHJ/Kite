d78357122525d6db74e7eac4016db0e53347fc63
AlBan <albaneo0724@gmail.com>
2020-02-02 00:06:54 +0900

    update file upload

--- Backend/imgserver/src/main/java/edu/wildbody/imgserver/Controller/FileController.java
+++ Backend/imgserver/src/main/java/edu/wildbody/imgserver/Controller/FileController.java
@@ -1,7 +1,11 @@
 package edu.wildbody.imgserver.Controller;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,26 +25,36 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public String upload(@RequestPart("images") MultipartFile file) {
-        String msg = "file upload fail";
+    public ResponseEntity<Map<String, Object>> upload(@RequestPart("images") MultipartFile file) {
+        String msg = null;
+        boolean isupload = false;
         String filename = file.getOriginalFilename();
         String conType = file.getContentType();
-
         System.out.println("receive file : " + filename + "  " + conType);
+
+        ResponseEntity<Map<String, Object>> ret = null;
+        Map<String, Object> map = new HashMap<String, Object>();
 
         if (!"image".contains(file.getContentType().split("/")[0])) {
             //에러 페이지
-            return "not allow file type";
+            msg = "not allow file type";
+            isupload = false;
+        } else {
+            try {
+                file.transferTo(new File(path, filename));
+                msg = "file upload success";
+                isupload = true;
+            } catch (Exception e) {
+                // 파일 업로드 에러
+                System.out.println(e.getMessage());
+                msg = "error with file uploading";
+            }
         }
 
-        try {
-            file.transferTo(new File(path, filename));
-            msg = "file upload success";
-        } catch (Exception e) {
-            // 파일 업로드 에러
-            System.out.println(e.getMessage());
-        }
+        map.put("isupload", isupload);
+        map.put("msg", msg);
+        ret = new ResponseEntity<>(map, HttpStatus.OK);
 
-        return msg;
+        return ret;
     }
 }
