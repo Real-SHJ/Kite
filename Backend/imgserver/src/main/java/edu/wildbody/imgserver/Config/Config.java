78e66b525cb3c90944694ffaf3fc62c6f21db324
AlBan <albaneo0724@gmail.com>
2020-02-01 23:43:53 +0900

    image upload implement

--- Backend/imgserver/src/main/java/edu/wildbody/imgserver/Config/Config.java
+++ Backend/imgserver/src/main/java/edu/wildbody/imgserver/Config/Config.java
@@ -3,12 +3,18 @@ package edu.wildbody.imgserver.Config;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+@EnableWebMvc
 public class Config implements WebMvcConfigurer {
 
     private final String uploadImagePath;
@@ -29,5 +35,14 @@ public class Config implements WebMvcConfigurer {
                 .addResolver(new PathResourceResolver());
 
         }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(36000);
     }
 }
--- Backend/imgserver/src/main/java/edu/wildbody/imgserver/Controller/FileController.java
+++ Backend/imgserver/src/main/java/edu/wildbody/imgserver/Controller/FileController.java
@@ -1,30 +1,44 @@
 package edu.wildbody.imgserver.Controller;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.util.List;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/api/file")
+@CrossOrigin("*")
 public class FileController {
 
-    private static final String prefix="/var/lib/img/";
+    private static String path = null;
 
-    @RequestMapping(value = "/multiupload",method = RequestMethod.POST)
-    public String upload(@RequestPart MultipartFile file){
-        String msg = null;
+    FileController(@Value("${file.upload.path}") String path) {
+        this.path = path;
+    }
 
-        try{
-        }catch (Exception e){
+    @PostMapping("/upload")
+    public String upload(@RequestPart("images") MultipartFile file) {
+        String msg = "file upload fail";
+        String filename = file.getOriginalFilename();
+        String conType = file.getContentType();
+
+        System.out.println("receive file : " + filename + "  " + conType);
+
+        if (!"image".contains(file.getContentType().split("/")[0])) {
+            //에러 페이지
+            return "not allow file type";
+        }
+
+        try {
+            file.transferTo(new File(path, filename));
+            msg = "file upload success";
+        } catch (Exception e) {
             // 파일 업로드 에러
+            System.out.println(e.getMessage());
         }
 
         return msg;
